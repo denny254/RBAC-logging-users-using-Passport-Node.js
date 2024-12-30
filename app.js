9d4ec868b5503f1975af9c8e78ce096803e8b883
Dennis Kinanga <denniskinanga6@gmail.com>
2024-12-30 18:09:24 +0300

    redirection of routes

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const session = require("express-session");
-const connectFlash = require("connect-flash");
 const passport = require("passport");
+const MongoStore = require("connect-mongo"); 
+const connectEnsureLogin = require("connect-ensure-login");
 
-//Initialization
+// Initialization
 const app = express();
 app.use(morgan("dev"));
 app.set("view engine", "ejs");
@@ -15,45 +16,44 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Init session
+// Init session with connect-mongo
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      // secure: true,
       httpOnly: true,
     },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+      dbName: process.env.DB_NAME,
+    }),
   })
 );
 
-//For Passport JS Authentication
+// For Passport JS Authentication
 app.use(passport.initialize());
 app.use(passport.session());
 require('./utils/passport.auth');
 
-app.use(connectFlash());
 app.use((req, res, next) => {
-  res.locals.messages = req.flash();
+  res.locals.user = req.user;
   next();
 });
 
 // Connect to MongoDB
 const ConnectDB = async () => {
-  let db = "";
-
+  let db = process.env.MONGODB_URI;
   if (process.env.Environment === "dev" || process.env.Environment === "test") {
     db = `${process.env.MONGODB_URI}/${process.env.DB_NAME}`;
-  } else {
-    db = `${process.env.MONGODB_URI}`;
   }
 
   try {
     await mongoose.connect(db);
-    console.log(`Connected....`);
+    console.log(`Connected to MongoDB`);
     app.listen(PORT, () => {
-      console.log(`on port ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Database connection failed:", error);
@@ -66,14 +66,22 @@ ConnectDB();
 // Routes
 app.use("/", require("./routes/index.route"));
 app.use("/auth", require("./routes/auth.route"));
-app.use("/user", require("./routes/user.route"));
+app.use("/user", connectEnsureLogin.ensureLoggedIn({redirectTo: '/auth/login'}), require("./routes/user.route"));
 
-app.use((req, res, next) => {});
+app.use((req, res, next) => {
+  next(createHttpError(404, "Not Found"));
+});
 
 app.use((error, req, res, next) => {
-  error.status = error.status || 500;
-  res.status(error.status);
+  res.status(error.status || 500);
   res.render("error_40x", { error });
 });
 
 const PORT = process.env.PORT || 3000;
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     return next();
+//   }
+//   res.redirect('/auth/login');
+// }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,76 +1,110 @@
-const router = require('express').Router()
-const User = require('../models/user.model')
-const { body, validationResult } = require('express-validator')
-const passport = require('passport')
-const { ensureLoggedOut } = require('connect-ensure-login');
+const router = require("express").Router();
+const User = require("../models/user.model");
+const { body, validationResult } = require("express-validator");
+const passport = require("passport");
+const connectEnsure = require("connect-ensure-login");
 
-router.get('/login', async(req, res, next)=>{
-    res.render('Login')
-})
+router.get(
+  "/login",
+  connectEnsure.ensureLoggedOut({ redirectTo: "/" }),
+  async (req, res, next) => {
+    res.render("Login");
+  }
+);
 
 router.post(
-    '/login',
-    // ensureLoggedOut({ redirectTo: '/' }),
-    passport.authenticate('local', {
-      successReturnToOrRedirect: '/user/profile',
-      failureRedirect: '/auth/login',
-      failureFlash: true,
-    })
-  );
+  "/login",
+  connectEnsure.ensureLoggedOut({ redirectTo: "/" }),
+  // ensureLoggedOut({ redirectTo: '/' }),
+  passport.authenticate("local", {
+    successReturnToOrRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+  })
+);
 
-router.get('/register', async(req, res, next)=>{
-    res.render('Register');
-})
+router.get("/register", connectEnsure.ensureLoggedOut({ redirectTo: "/" }), async (req, res, next) => {
+  res.render("Register");
+});
 
-
-router.post('/register',[
-    body('email').trim().isEmail().withMessage('Email must be a valid email')
-        .normalizeEmail().toLowerCase(),
-    body('password').trim().isLength(2).withMessage('Password length short, min 2 char required'),
-    body('password2').custom((value, {req}) => {
-        if(value !== req.body.password){
-            throw new Error('Passwords do not match')
-        }
-        return true;
-    })
-],
-async(req, res, next)=>{
+router.post(
+  "/register",
+  connectEnsure.ensureLoggedOut({ redirectTo: "/" }),
+  [
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Email must be a valid email")
+      .normalizeEmail()
+      .toLowerCase(),
+    body("password")
+      .trim()
+      .isLength(2)
+      .withMessage("Password length short, min 2 char required"),
+    body("password2").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+  ],
+  async (req, res, next) => {
     try {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
-            errors.array().forEach(error =>{
-                req.flash('error', error.msg)
-            })
-            res.render('register', {
-                email: req.body.email, 
-                messages: req.flash()})
-            return;
-        }
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        errors.array().forEach((error) => {
+          req.flash("error", error.msg);
+        });
+        res.render("register", {
+          email: req.body.email,
+          messages: req.flash(),
+        });
+        return;
+      }
 
-        const { email } = req.body;
-        const doesExist = await User.findOne({ email });
-        if(doesExist){
-            res.redirect("/auth/register");
-            return;
-        }
-        const user = new User(req.body)
-        await user.save();
-        
-        req.flash('success', `${user.email} registered successfully, you can now login`)
-        res.redirect('/auth/login');
+      const { email } = req.body;
+      const doesExist = await User.findOne({ email });
+      if (doesExist) {
+        res.redirect("/auth/register");
+        return;
+      }
+      const user = new User(req.body);
+      await user.save();
+
+      req.flash(
+        "success",
+        `${user.email} registered successfully, you can now login`
+      );
+      res.redirect("/auth/login");
     } catch (error) {
-        next(error);
+      next(error);
     }
-});
+  }
+);
 
-router.get('/logout', async (req, res, next) => {
-    req.logout((err) => {
-        if (err) {
-            return next(err); // Pass the error to the next middleware
-        }
-        res.redirect('/'); // Redirect to the homepage after successful logout
-    });
+router.get("/logout", connectEnsure.ensureLoggedIn({redirectTo: '/'}), async (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err); // Pass the error to the next middleware
+    }
+    res.redirect("/"); // Redirect to the homepage after successful logout
+  });
 });
-
 
 module.exports = router;
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     next();
+//   } else {
+//     res.redirect("/auth/login");
+//   }
+// }
+
+// function ensureNOTAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     res.redirect("back");
+//   } else {
+//     next();
+//   }
+// }
